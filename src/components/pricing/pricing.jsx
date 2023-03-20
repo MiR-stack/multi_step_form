735eafcca25c13613a1de714b700b6540d8643b4
habibmir808 <habibmir808@gmail.com>
2023-03-21 02:11:26 +0600

    remove unused console log

--- src/components/pricing/pricing.jsx
+++ src/components/pricing/pricing.jsx
@@ -19,31 +19,22 @@ import Layout from "../layout";
 import FormActions from "../formActions";
 import { useEffect, useState } from "react";
 
-function Pricing({step,storedData,handleData,next,handleBack}) {
+function Pricing({ step, storedData, handleData, next, handleBack }) {
+  const [period, setPeriod] = useState(storedData?.period || "mo");
 
-    console.log(storedData)
-
-    const [period ,setPeriod] = useState(storedData?.period || 'mo')
-
-    const handlePeriod = (period) =>{
-        if(period === 'mo'){
-            setPeriod('yr')
-        }else{
-            setPeriod('mo')
-        }
+  const handlePeriod = (period) => {
+    if (period === "mo") {
+      setPeriod("yr");
+    } else {
+      setPeriod("mo");
     }
+  };
 
-    console.log(storedData)
+  const [pricing, setPricing] = useState(storedData?.pricing || data.state[0]);
 
-    const [pricing,setPricing] = useState(storedData?.pricing ||data.state[0])
-
-    
-
-    useEffect(()=>{
-        handleData({pricing,period})
-    },[pricing,period])
-
-   
+  useEffect(() => {
+    handleData({ pricing, period });
+  }, [pricing, period]);
 
   return (
     <Container>
@@ -51,23 +42,37 @@ function Pricing({step,storedData,handleData,next,handleBack}) {
         <Box>
           <Wraper>
             {data.state.map((item) => (
-              <Card key={item.name} active={pricing.name === item.name} onClick={()=>{setPricing(item)}}>
+              <Card
+                key={item.name}
+                active={pricing.name === item.name}
+                onClick={() => {
+                  setPricing(item);
+                }}
+              >
                 <img src={item.icon} alt="item.name" />
                 <Content>
                   <Name>{item.name} </Name>
-                  <Price>${item.price[period].value}/{period} </Price>
+                  <Price>
+                    ${item.price[period].value}/{period}{" "}
+                  </Price>
                   <Offer>{item.price[period].offer} </Offer>
                 </Content>
               </Card>
             ))}
           </Wraper>
           <SwitchBox>
-            <Text active={period === 'mo'} >monthly </Text>
+            <Text active={period === "mo"}>monthly </Text>
             <Switch>
-                <Input type='checkbox' checked={period === 'yr'}  onChange={()=>{handlePeriod(period)}} />
-                <Slider />
+              <Input
+                type="checkbox"
+                checked={period === "yr"}
+                onChange={() => {
+                  handlePeriod(period);
+                }}
+              />
+              <Slider />
             </Switch>
-            <Text active={period === 'yr'} >yearly</Text>
+            <Text active={period === "yr"}>yearly</Text>
           </SwitchBox>
         </Box>
       </Layout>
